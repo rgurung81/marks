f7eaef233b858ee7e86edc95d79d0612cb06f29b
rgurung81 <radpter@hotmail.com>
2025-06-07 14:05:49 +0100

    Update marks.py

--- marks.py
+++ marks.py
@@ -3,18 +3,20 @@
 
 print('Marks in 5 subject:97,90,100,95,98')
 
+#def the function
 def calculatesum(a,b,c,d,e):
   C=a+b+c+d+e
   return C
-
+#call the function
 print(f'Total marks:{calculatesum(97,90,100,95,98)}')
 
-                                 
-def calculatepercentage(a,b,c,d,e):
+#def the function
+def calculatepercentage(a,b,c,d,e):  
     P=(a+b+c+d+e)/500*100
     return P
-    
-print(f'Percentage:{calculatepercentage(97,90,100,95,98)}%')
+
+#call the function
+print(f'Percentage:{calculatepercentage(90,90,60,95,98)}%')
 
 
 (Percentage)=96
